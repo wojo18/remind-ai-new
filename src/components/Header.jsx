ece364092898ae675f21970c6a67386c52ec0bdb
wojo18 <wojti_ww@interia.pl>
2025-07-27 19:36:46 +0200

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header style={{ padding: "1rem", backgroundColor: "#f5f5f5" }}>
+      <h1>Remind-AI</h1>
+    </header>
+  );
+}
